47d16f7bbe2c5794675e0e0f62945d6205e7eaac
CherryNine <binarybrawler7@gmail.com>
2024-09-19 23:03:49 +0300

    feat:enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3001;
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+    credentials: true,
+  });
   await app.listen(PORT);
 }
 bootstrap();
